178033900653f3dc72e0240aa3283104d964cc0b
Joe Pearson <joe.pearson@avid.com>
2021-07-05 16:36:49 -0700

    Cleanup

--- lib/azure.ex
+++ lib/azure.ex
@@ -2,17 +2,4 @@ defmodule Azure do
   @moduledoc """
   Documentation for `Azure`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Azure.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
