5276dd8d616596ea3283f01afa556376cfba2e06
Nulian <peter.arentsen@outlook.com>
2022-01-17 16:50:23 +0100

    loosen mix requirements

--- mix.exs
+++ mix.exs
@@ -30,18 +30,17 @@ defmodule Azure.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.5.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:ex_machina, only: [:dev, :test]},
-      {:hackney, "~> 1.17.4"},
-      {:jason, "~> 1.2.2", optional: true},
-      {:named_args, "~> 0.1.1"},
+      {:ex_machina, ">= 0.0.0", only: [:dev, :test]},
+      {:hackney, "~> 1.17"},
+      {:jason, "~> 1.2", optional: true},
       {:poison, ">= 1.0.0", optional: true},
-      {:sweet_xml, "~> 0.7.0"},
-      {:tesla, "~> 1.4.1"},
-      {:timex, "~> 3.7.5"},
-      {:xml_builder, "~> 2.2.0"}
+      {:sweet_xml, "~> 0.7"},
+      {:tesla, "~> 1.4"},
+      {:timex, "~> 3.7"},
+      {:xml_builder, "~> 2.2"}
     ]
   end
 
